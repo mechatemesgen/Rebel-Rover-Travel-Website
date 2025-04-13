2686e0b3f2aba2f978914881394e92cbd3bda3ef
mechatemesgen <ililnaafbarihe94@gmail.com>
2025-04-14 02:26:59 +0300

    Contact Us

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -19,7 +19,7 @@ export default function Contact() {
      <div className="hero bg-cover bg-center text-white p-8 sm:p-12 lg:p-20 min-h-screen flex flex-col items-center justify-center"
       style={{ backgroundImage: `url(${Horobanner})` }}>
           <h1 className="text-5xl sm:text-6xl lg:text-6xl font-bold" style={{ textShadow: '4px 4.5px 2px rgba(0, 0, 0, 0.2)' }}>
-            About Us
+            Contact Us
           </h1>
           <p className="pt-4 text-xl sm:text-2xl lg:text-2.5xl font-bold" style={{ textShadow: '4px 4.5px 2px rgba(0, 0, 0, 0.2)' }}>
             Home &gt; Contact Us
