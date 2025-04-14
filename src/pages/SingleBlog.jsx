6f0abfec1521ad5a1b3bb67ff5ae85d50df17355
mechatemesgen <ililnaafbarihe94@gmail.com>
2025-04-14 16:19:51 +0300

    update blog page

--- src/pages/SingleBlog.jsx
+++ src/pages/SingleBlog.jsx
@@ -5,6 +5,7 @@ import Header from "../components/Blog/Header";
 import Content1 from "../components/Blog/Content1";
 import Content2 from "../components/Blog/Content2";
 import Image2 from "../assets/image2.png"; 
+
 const SingleBlog = () => {
   const [destination, setDestination] = useState(null);
   const { id } = useParams();
@@ -16,7 +17,6 @@ const SingleBlog = () => {
         `https://67eadc5834bcedd95f64c9f3.mockapi.io/RebelRover/Destinations/1`
       )
       .then((response) => {
-        // console.log(response.data);
         setDestination(response.data);
       })
       .catch((error) => {
@@ -29,7 +29,7 @@ const SingleBlog = () => {
   }, [currentPage]);
   
   return (
-    <>
+    <div className="overflow-x-hidden"> {/* Prevent horizontal scroll globally */}
       <Header />
       {destination ? (
         <Content1 destination={destination} />
@@ -37,7 +37,7 @@ const SingleBlog = () => {
         <p className="font-semibold text-center">Loading...</p>
       )}
       <Content2 />
-    </>
+    </div>
   );
 };
 
